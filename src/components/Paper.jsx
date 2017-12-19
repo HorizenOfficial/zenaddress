bf00637a53d585c54585c3e58165be8a62e39138
Gniar <Gniar@protonmail.com>
2017-12-19 13:05:25 +0100

    update text in paper wallet tab

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -114,24 +114,13 @@ class Paper extends Component {
                 <Row className="r3">
                     <Col>
                         <p>
-                            <b>A Zencash wallet</b> is as simple as a single pairing of a Zencash address with its corresponding Zencash private key. You can share your address to receive ZEN payments, however your private key is what allows you to unlock and manage your funds, keep it safe.
+                          A paper wallet is a piece of paper containing a public address and a private key. It allows you to store some ZENs offline.
                         </p>
                         <p>
-                            <b>To safeguard this wallet</b> you must print or otherwise record the Zencash address and private key. It is important to make a backup copy of the private key and store it in a safe location. This site does not have knowledge of your private key. If you leave/refresh the site or press the "Generate New Address" button then a new private key will be generated and the previously displayed private key will not be retrievable. Your Zencash private key should be kept a secret. Whomever you share the private key with has access to spend all the ZEN associated with that address. If you print your wallet then store it in a zip lock bag to keep it safe from water. Treat a paper wallet like cash.
+                          These kind of wallets are vulnerable to loss and theft. You should keep it safe like jewels or cash. Therefore it is recommended either to have a backup or to generate it only for a temporary use.
                         </p>
                         <p>
-                            <b>Add funds</b> to this wallet by instructing others to send ZEN to your Zen address.
-                        </p>
-                        <p>
-                            <b>Check your balance</b> by entering your Zen address on one of these explorers :
-                        </p>
-                        <ul style={{listStyleType: 'none'}}>
-                            <li>https://explorer.zensystem.io/</li>
-                            <li>http://explorer.zenmine.pro/insight/</li>
-                            <li>https://explorer.zen-solutions.io/</li>
-                        </ul>
-                        <p>
-                            <b>To spend your ZEN</b> you can download the <a href='https://github.com/ZencashOfficial/zencash-swing-wallet-ui/releases'>Zencash Swing Wallet</a> and import your private key to the p2p client wallet.
+                          <b>Image Quality:</b> print this wallet with Chrome for a better quality.
                         </p>
                     </Col>
                 </Row>
